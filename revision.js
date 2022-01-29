2d43a42251dec1d6eb79e5ca776c4ea797e5d3f0
Sumit <sumit14122000@gmail.com>
2022-01-29 11:57:31 +0530

    initial stage

---
+++ revision.js
@@ -0,0 +1,11 @@
+var a,b;
+function calculator(a,b) 
+{
+    console.log("addition",a+b)
+    console.log("addition",a-b)
+    console.log("addition",a*b)
+    console.log("addition",a/b)
+
+}
+var c=calculator(2,4);
+console.log("output is: ",c);
